ca1762c426a5cb43e3564da753fb12eada862272
Juan Pinol <JuannnCodes@gmail.com>
2022-12-14 20:15:37 -0500

    Forgot the babel config file, added a background image and basic html/css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./index.css";
 
 export default function App () {
 
   return (
-    <div>
-      Hello World!
+    <div class="bgimg">
+      <div class="topleft">
+        <p>Logo</p>
+      </div>
+      <div class="middle">
+        <h1>COMING SOON</h1>
+        <hr />
+      </div>
+      <div class="bottomleft">
+        <p>Juan is working on his portfolio diligently.</p>
+      </div>
     </div>
   )
 }
